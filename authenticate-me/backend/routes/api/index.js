474fa7eba9d8e4f316b177388798dc2f2263f52b
Antonio Davila-Olivares <ajd57@nau.edu>
2021-07-15 13:14:14 -0700

    completed phase 3

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,9 +1,13 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
 
 
 module.exports = router;
