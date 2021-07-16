58a74b89670b3531aee059b5ea24502a992495d5
Antonio Davila-Olivares <ajd57@nau.edu>
2021-07-16 16:13:14 -0700

    authenticate me completed

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -34,7 +34,6 @@ router.post(
     validateSignup,
     asyncHandler(async (req, res) => {
         const { email, password, username } = req.body;
-        console.log('LOOK HERE +++++++++++', { email, password, username })
         const user = await User.signup({ email, username, password });
 
         await setTokenCookie(res, user);
