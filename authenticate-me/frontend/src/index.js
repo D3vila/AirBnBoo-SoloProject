f3ffdf928ccf9272f63c3d44408ea0e381ca4429
Antonio Davila-Olivares <ajd57@nau.edu>
2021-07-15 17:06:39 -0700

    loginForm renders on App

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
-  window.sessionActions = sessionActions;
+    window.sessionActions = sessionActions;
 }
 
 function Root() {
