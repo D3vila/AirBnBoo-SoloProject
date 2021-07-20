63943deed895b51e49ab33d9b58ef35f3d3c002e
Antonio Davila-Olivares <ajd57@nau.edu>
2021-07-19 17:19:02 -0700

    completed DB Associations

---
+++ authenticate-me/backend/db/migrations/20210719214109-create-spot.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING(150)
+      },
+      lat: {
+        allowNull: false,
+        type: Sequelize.FLOAT
+      },
+      lng: {
+        allowNull: false,
+        type: Sequelize.FLOAT
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      img: {
+        allowNull: true,
+        type: Sequelize.STRING(1000)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Spots');
+  }
+};
---
+++ authenticate-me/backend/db/migrations/20210719214849-create-booking.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      startDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      endDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Bookings');
+  }
+};
