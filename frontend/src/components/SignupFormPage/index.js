e18eb38056df404ad3e4873936af298010665634
Antonio Davila-Olivares <ajd57@nau.edu>
2021-07-24 14:47:34 -0700

    working on signup page css

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -3,6 +3,7 @@ import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import './SignupForm.css';
+import lightHouse from '../videos/oceanView-lighthouse.mp4'
 
 function SignupFormPage() {
     const dispatch = useDispatch();
@@ -31,48 +32,84 @@ function SignupFormPage() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-            </ul>
-            <label>
-                Username
-                <input
-                    type='text'
-                    value={username}
-                    onChange={(e) => setUserName(e.target.value)}
+        <>
+            <div className='split__screen'>
+                <div className='left__side'>
+                    <section className='half__page'>
+                        <div className='video__container1'>
+                            <video className='lighthouse__vid' loop autoPlay muted>
+                                <source src={lightHouse} type='video/mp4' />
+                            </video>
+                            <div className='overlay1'>
+                                <h1 className='left__title'>Explore the paranormal side of traveling</h1>
+                                <p className='left__subTitle'>Over 100,000 of locations all over the world.</p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <div className='right__side'>
+                    <form className='signup__form' onSubmit={handleSubmit}>
+                        <section className='half__page'>
+                            <h2 className='right__title'>Sign Up</h2>
+                        </section>
+                        <div>
+                            <ul>
+                                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                            </ul>
+                        </div>
+                        <div>
+                            <label>
+                                Username
+                                <input
+                                    type='text'
+                                    value={username}
+                                    onChange={(e) => setUserName(e.target.value)}
 
-                />
-            </label>
-            <label>
-                Email
-                <input
-                    type='text'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                Email
+                                <input
+                                    type='text'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
 
-                />
-            </label>
-            <label>
-                Password
-                <input
-                    type='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                Password
+                                <input
+                                    type='password'
+                                    value={password}
+                                    placeholder='Must be at least 6 characters'
+                                    onChange={(e) => setPassword(e.target.value)}
 
-                />
-            </label>
-            <label>
-                Confirm Password
-                <input
-                    type='text'
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    
-                />
-            </label>
-            <button type='submit'>Sign Up</button>
-        </form>
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                Confirm Password
+                                <input
+                                    type='text'
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+
+                                />
+                            </label>
+                        </div>
+                        <button className='signup__button' type='submit'>Sign Up</button>
+                    </form>
+
+                </div>
+
+
+            </div>
+        </>
     );
 };
 
